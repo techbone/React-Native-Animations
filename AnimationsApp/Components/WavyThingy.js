b3efe2ed33b3cabc59fdb20588a3a2ecfbbe6549
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-08-18 21:50:03 +0100

    fix: sorted my WavyThingy into a component

---
+++ AnimationsApp/Components/WavyThingy.js
@@ -0,0 +1,38 @@
+import { Feather } from "@expo/vector-icons";
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+
+const _color = "#6E01EF";
+const _size = 100;
+
+const WavyThingy = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View style={[styles.dot, styles.center]}>
+        {/* {[...Array(3).keys()].map} */}
+        <Feather name="phone-outgoing" size={32} color="#fff" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  dot: {
+    width: _size,
+    height: _size,
+    borderRadius: _size,
+    backgroundColor: _color,
+  },
+  center: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default WavyThingy;
--- App.js
+++ App.js
@@ -1,38 +1,13 @@
 import { Feather } from "@expo/vector-icons";
 import * as React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import WavyThingy from "./AnimationsApp/Components/WavyThingy";
 
 const _color = "#6E01EF";
 const _size = 100;
 
-const WavyThingy = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <View style={[styles.dot, styles.center]}>
-        {/* {[...Array(3).keys()].map} */}
-        <Feather name="phone-outgoing" size={32} color="#fff" />
-      </View>
-    </View>
-  );
+const App = () => {
+  return <WavyThingy />;
 };
 
-const styles = StyleSheet.create({
-  dot: {
-    width: _size,
-    height: _size,
-    borderRadius: _size,
-    backgroundColor: _color,
-  },
-  center: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-export default WavyThingy;
+export default App;
