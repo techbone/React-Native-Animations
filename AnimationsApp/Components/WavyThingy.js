cc537b6f1951f5beeb230ac9716d244b122a3bab
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-08-18 23:54:49 +0100

    feat: animation not working yet

--- AnimationsApp/Components/WavyThingy.js
+++ AnimationsApp/Components/WavyThingy.js
@@ -1,6 +1,8 @@
 import { Feather } from "@expo/vector-icons";
+import { MotiView } from "@motify/components";
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
+import { Easing } from "react-native-reanimated";
 
 const _color = "#6E01EF";
 const _size = 100;
@@ -16,7 +18,14 @@ const WavyThingy = () => {
     >
       <View style={[styles.dot, styles.center]}>
         {[...Array(3).keys()].map((index) => {
-          <View
+          <MotiView
+            from={{ opacity: 1, scale: 1 }}
+            animate={{ opacity: 0, scale: 4 }}
+            transition={{
+              type: "timing",
+              duration: 2000,
+              easing: Easing.out(Easing.ease),
+            }}
             key={index}
             style={[StyleSheet.absoluteFillObject, styles.dot]}
           />;
